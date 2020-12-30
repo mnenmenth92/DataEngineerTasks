25b8ec569008e9025b9b2d26a9989322be8fbf71
unknown <msuch92@gmail.com>
2020-12-30 07:02:29 +0100

    data

---
+++ dataset/tables.sql
@@ -0,0 +1,2 @@
+CREATE TABLE users (user_id int, age varchar(5), country varchar(2));
+CREATE TABLE user_interaction (event_id varchar(36), user_id int, event_type varchar(5), event_time timestamp);
